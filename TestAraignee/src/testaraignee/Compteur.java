5cf42c02d2b0be55b985ba29c0ccecbf4f73602d
AubeD <o.bs@hotmail.fr>
2017-09-28 11:56:50 +0200

    tout

--- TestAraignee/src/testaraignee/Compteur.java
+++ TestAraignee/src/testaraignee/Compteur.java
@@ -9,18 +9,31 @@ import java.awt.event.ActionListener;
 import javax.swing.ButtonGroup;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 /**
  *
- * @author Océane
+ * @author OcÃ©ane
  */
 public class Compteur implements ActionListener {
     private int compteur;
-    private int[][] matrice; // Cette matrice permet de stocker l'état du jeu (les pions sont où et à qui)
+    private int[][] matrice; // Cette matrice permet de stocker l'Ã©tat du jeu (les pions sont oÃ¹ et Ã  qui)
+    private int [][] Selection;
+    private ArrayList<JButton> ListeBoutons;
 
     public Compteur(){
         compteur=0;
-        matrice= new int[7][7]; //Il y a la matrice 3x3 plus 2 rangées de cases tout autour (ce qui permet de ne pas avoir d'érreur lorsqu'on vérifie si l'état est gagnant
+        Selection=new int[1][2];
+        matrice= new int[7][7]; //Il y a la matrice 3x3 plus 2 rangÃ©es de cases tout autour (ce qui permet de ne pas avoir d'Ã©rreur lorsqu'on vÃ©rifie si l'Ã©tat est gagnant
+        ListeBoutons = new ArrayList(); //On crÃ©e la liste contenant les 9 boutons
+        for (int i = 0; i < 9; i++) {
+            ListeBoutons.add(new JButton(""));
+        }
     }
+
+    public ArrayList<JButton> getListeBoutons() {
+        return ListeBoutons;
+    }
+    
     
     public int getCompteur() {
         return compteur;
@@ -34,10 +47,22 @@ public class Compteur implements ActionListener {
         return matrice;
     }
 
+    public int[][] getSelection() {
+        return Selection;
+    }
+
+    public void setSelection(int[][] Selection) {
+        this.Selection = Selection;
+    }
+
+    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         
     }
+
+   
     
    
     
--- TestAraignee/src/testaraignee/TraitementB.java
+++ TestAraignee/src/testaraignee/TraitementB.java
@@ -10,16 +10,18 @@ import javax.swing.*;
 import java.awt.*;
 import javax.imageio.*;
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  *
- * @author Océane
+ * @author OcÃ©ane
  */
 public class TraitementB extends Compteur {
     private JButton button;
     private boolean actif;
     private Compteur compteur;
     private int[][] grille; // C'est la matrice dans Compteur (voir classe Compteur)
+    private int[][] Selection; //matrice qui donne les coordonnees d'un pion selectionnÃ©
     private int ord;
     private int abs;
     private JFrame frame;
@@ -30,11 +32,25 @@ public class TraitementB extends Compteur {
         compteur=compt;
         compteur.setCompteur(0);
         grille=compteur.getMatrice();
+        Selection=compteur.getSelection();
         actif=false;
-        ord=xi+1;
-        abs=xj+1;
+        ord=xj+1;
+        abs=xi+1;
         frame=f;
         
+    }
+    public void setImage(String adresse){ //on crÃ©e une fonction pour associer une image Ã  un bouton
+        Image img= null;
+                try {
+                img = ImageIO.read(new File(adresse));
+                } catch (IOException i) {
+                }// il faut faire une exception pour aller chercher un fichier au cas oÃ¹ le fichier n'existe pas
+                Image newimg = img.getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH); // changer la taille de l'image pour s'adapter au bouton
+                ImageIcon icon = new ImageIcon(newimg);
+                button.setIcon(icon); // ajouter l'image au bouton
+                button.setOpaque(false); 
+                button.setContentAreaFilled(false);
+                button.setBorderPainted(false); // Cela remet le bouton invisible
     }
     
     @Override
@@ -50,46 +66,79 @@ public class TraitementB extends Compteur {
             actif=true;
             compteur.setCompteur(c);
             if (c%2==0){
-                Image img= null;
-                try {
-                img = ImageIO.read(new File("src/ressources/Bouton bleu.png"));
-                } catch (IOException i) {
-                }// il faut faire une exception pour aller chercher un fichier au cas où le fichier n'existe pas
-                Image newimg = img.getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH); // changer la taille de l'image pour s'adapter au bouton
-                ImageIcon icon = new ImageIcon(newimg);
-                button.setIcon(icon); // ajouter l'image au bouton
-                button.setOpaque(false); 
-                button.setContentAreaFilled(false);
-                button.setBorderPainted(false); // Cela remet le bouton invisible
-                grille[ord][abs]=2; // Signale que cette case  est désormais occupée par le joueur 2
+                this.setImage("src/ressources/Bouton bleu.png");
+                grille[ord][abs]=2; // Signale que cette case  est dÃ©sormais occupÃ©e par le joueur 2
                 joueur=2; // Le joueur de ce tour est le joueur 2
             }
             else{
-                Image img= null;
-                try {
-                img = ImageIO.read(new File("src/ressources/Bouton rouge.png"));
-                } catch (IOException i) {
-                }
-                Image newimg = img.getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH);
-                ImageIcon icon = new ImageIcon(newimg);
-                button.setIcon(icon);
-                button.setOpaque(false);
-                button.setContentAreaFilled(false);
-                button.setBorderPainted(false);
+                this.setImage("src/ressources/Bouton rouge.png");
                 grille[ord][abs]=1;
                 joueur=1;
             }
             
         }
         }
+        if (c>6){
+            if (Selection[0][0]!=0){ //cas oÃ¹ un bouton est selectionnÃ©
+                if (c%2==0){
+                    this.setImage("src/ressources/Bouton bleu.png");
+                    actif=true;
+                    compteur.getListeBoutons().get(3*Selection[0][0]+Selection[0][1]-4).setIcon(null); // ajouter l'image au bouton
+                    compteur.getListeBoutons().get(3*Selection[0][0]+Selection[0][1]-4).setOpaque(false); 
+                    compteur.getListeBoutons().get(3*Selection[0][0]+Selection[0][1]-4).setContentAreaFilled(false);
+                    compteur.getListeBoutons().get(3*Selection[0][0]+Selection[0][1]-4).setBorderPainted(false); // Cela remet le bouton invisible
+                    Selection[0][0]=0;
+                    grille[Selection[0][0]][Selection[0][1]]=0;
+                    compteur.setCompteur(c);
+                    joueur=2; // Le joueur de ce tour est le joueur 2
+                }
+                else{
+                    this.setImage("src/ressources/Bouton rouge.png");
+                    actif=true;     
+                    compteur.getListeBoutons().get(3*Selection[0][0]+Selection[0][1]-4).setIcon(null); // ajouter l'image au bouton
+                    compteur.getListeBoutons().get(3*Selection[0][0]+Selection[0][1]-4).setOpaque(false); 
+                    compteur.getListeBoutons().get(3*Selection[0][0]+Selection[0][1]-4).setContentAreaFilled(false);
+                    compteur.getListeBoutons().get(3*Selection[0][0]+Selection[0][1]-4).setBorderPainted(false); // Cela remet le bouton invisible    
+                    Selection[0][0]=0;
+                    compteur.setCompteur(c);
+                    grille[Selection[0][0]][Selection[0][1]]=0;
+                    joueur=1; // Le joueur de ce tour est le joueur 1
+                }                              
+            }
+            else{
+                if (Selection[0][0]==0 && actif==false){
+                    JOptionPane.showMessageDialog(frame,"Selectionnez un de vos pions pour le deplacer");     
+                }
+                if (Selection[0][0]==0 && grille[ord][abs]==c%2+1){
+                    JOptionPane.showMessageDialog(frame,"Ceci est un pion adverse, recommencez !");
+                }
+                if (Selection[0][0]==0 && actif!=false && grille[ord][abs]!=c%2+1){
+                    if (c%2==0){
+                        this.setImage("src/ressources/Bouton bleu fonce.png");
+                        Selection[0][1]=ord-1;
+                        Selection[0][0]=abs-1;                   
+                    }
+                    else{
+                        this.setImage("src/ressources/Bouton rouge fonce.png");
+                        Selection[0][1]=ord-1;
+                        Selection[0][0]=abs-1;                    
+                    }
+                }
+            }
+            
+        }
+        
+        
         int NumeroJoueurSuivant=c%2+1;
         // Conditions gagnantes :
         if ((grille[ord-1][abs]==joueur&&(grille[ord-2][abs]==joueur||grille[ord+1][abs]==joueur))||(grille[ord+1][abs]==joueur&&(grille[ord+2][abs]==joueur||grille[ord-1][abs]==joueur))||(grille[ord][abs-1]==joueur&&(grille[ord][abs-2]==joueur||grille[ord][abs+1]==joueur))||(grille[ord][abs+1]==joueur&&(grille[ord][abs+2]==joueur||grille[ord][abs-1]==joueur))||(grille[ord-1][abs-1]==joueur&&(grille[ord-2][abs-2]==joueur||grille[ord+1][abs+1]==joueur))){
-            System.out.println("Gagné joueur "+joueur);
-            JOptionPane.showMessageDialog(frame,"Le joueur "+joueur+" a gagné !"); 
+            if (joueur!=0){
+                System.out.println("GagnÃ© joueur "+joueur);
+                JOptionPane.showMessageDialog(frame,"Le joueur "+joueur+" a gagnÃ© !"); 
+            }
         }      
         else
             JOptionPane.showMessageDialog(frame, "Au tour du joueur "+NumeroJoueurSuivant);
-        frame.repaint();
+        
     }
 }
